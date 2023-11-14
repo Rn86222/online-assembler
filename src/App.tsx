4002cfe6a18697ab8570bcaa95d3f1831d47927f
Rn86222 <rn86222.light0213@gmail.com>
2023-11-15 04:08:35 +0900

    assembler

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,51 @@
 
 import './App.css'
 
-import React, { useState } from 'react';
-import init from 'assembler-by-wasm';
+import React, { useState, useEffect } from 'react';
+import init, { assemble } from 'assembler-by-wasm';
 
 const App: React.FC = () => {
   const [inputText, setInputText] = useState<string>('');
-  const wasm = import('assembler-by-wasm');
-  wasm.then((wasm) => {
-    init(wasm).then(() => {
-      console.log(wasm.assemble("addi x1, x0, 1", "2"));
-    });
-  });
+
+  const [displayedText, setDisplayedText] = useState<string>('');
+
+  useEffect(() => {
+    // テキストが変更されるたびに非同期関数を呼び出す
+    const fetchData = async () => {
+      await init('node_modules/assembler-by-wasm/assembler_by_wasm_bg.wasm');
+      // 非同期処理が完了した後にテキストを表示する状態を更新
+      setDisplayedText(assemble(inputText, "16"));
+    };
+
+    fetchData();
+  }, [inputText]); // inputTextが変更された時だけuseEffectを実行
+
+
+  const renderMultilineText = (text: string) => {
+    return text.split('\n').map((line, index) => (
+      <React.Fragment key={index}>
+        {line}
+        <br />
+      </React.Fragment>
+    ));
+  };
 
   return (
     <div>
-      {}
-      <input
-        type="text"
-        value={inputText}
-        onChange={(e) => {
-          setInputText(e.target.value);
-        }}
-      />
+      <div style={{ display: 'flex', gap: '10px' }}>
+        {}
+        <textarea
+          value={inputText}
+          rows={10}
+          cols={30}
+          style={{ resize: 'none', overflow: 'scroll', marginRight: '10px' }}
+          onChange={(e) => setInputText(e.target.value)}
+        />
 
-      {}
-      <div style={{ marginTop: '10px' }}>
-        <strong>入力されたテキスト:</strong> {inputText}
+        {}
+        <div style={{ marginTop: '10px' }}>
+          <strong>アセンブル結果<br/></strong> {renderMultilineText(displayedText)}
+        </div>
       </div>
     </div>
   );
